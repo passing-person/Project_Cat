e2d4758b9fc18f04bfd435eb2a4fdb1c3ad5c50c
Julliane-Natalie-Stingray <pwdforgotten.orz@gmail.com>
2026-07-02 12:55:02 +0800

    -

--- Assets/Scripts/03_NPCAI/NPC Core/NpcAnim.cs
+++ Assets/Scripts/03_NPCAI/NPC Core/NpcAnim.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class NpcAnim : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

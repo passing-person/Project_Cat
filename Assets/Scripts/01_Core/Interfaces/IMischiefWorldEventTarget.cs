6854fce53407825cd9b27eb0e203f87e278c3b00
passing-person <51860204+passing-person@users.noreply.github.com>
2026-07-05 13:58:34 +0800

    bug patch

--- Assets/Scripts/01_Core/Interfaces/IMischiefWorldEventTarget.cs
+++ Assets/Scripts/01_Core/Interfaces/IMischiefWorldEventTarget.cs
@@ -3,6 +3,7 @@ public interface IMischiefWorldEventTarget
     string WorldEventTargetId { get; }
     MischiefWorldEventType WorldEventType { get; }
     bool CanStartWorldEvent();
+    string GetUnavailableReason();
     void OnWorldEventStarted(MischiefWorldEventContext context);
     void OnWorldEventCompleted(bool disableTarget, float cooldownDuration);
 }
